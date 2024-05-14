0473e9e83d0f8f054012114edd30c40abd4c71ed
Rizal Fauzi <rizalfauzi774@gmail.com>
2024-05-14 16:42:11 +0700

    [INITIAL] initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import viteLogo from '/vite.svg'
+// import '/styles/css/index.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
